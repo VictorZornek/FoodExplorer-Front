36f3da2676c4530759a63c2f6fba80ec097234ca
Victor Zornek <vzornek@example.com>
2025-02-26 16:01:14 -0300

    adicionando theme.js no projeto

---
+++ src/styles/theme.js
@@ -0,0 +1,37 @@
+export default {
+    COLORS: {
+        DARK: "#000405",
+        DARK_900: "#00070A",
+        DARK_800: "#000204",
+        DARK_700: "#000A0F",
+        DARK_600: "#000C12",
+        DARK_500: "#00111A",
+        DARK_400: "#001119",
+        DARK_300: "#0D161B",
+        DARK_200: "#0D1D25",
+        DARK_100: "#192227",
+    
+        WHITE: "#FFFFFF",
+        WHITE_100: "#FFFAF1",
+        WHITE_200: "#E1E1E6",
+
+        GRAY_100: "#C4C4CC",
+        GRAY_200: "#7C7C8A",
+        GRAY_400: "#76797B",
+        GRAY_700: "#4D585E",
+
+
+        ORANGE: "#FBA94C",
+
+        BLUE_LIGTH: "#82F3FF",
+        BLUE: "#065E7C",
+    
+    
+        RED_400: "#750310",
+        RED_300: "#92000E",
+        RED_200: "#AB222E",
+        RED_100: "#AB4D55",
+
+        GREEN: "#04D361",
+    }
+  }
